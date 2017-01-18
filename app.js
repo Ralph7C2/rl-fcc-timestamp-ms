b7c071676d229ad8a51153898d81c7e2ed061a63
ralph7c2 <ralph7c2@gmail.com>
2017-01-18 17:22:05 -0600

    api

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 const express = require('express');
 var app = express();
 
+app.get('/', function(req, res) {
+	res.sendFile(__dirname+"/index.html");
+});
+
 app.get('/:input', function(req, res) {
-	res.send(req.params.input);
+	var inp = req.params.input;
+	var ms = parseInt(inp);
+	if(isNaN(ms)) {
+		var d = new Date(inp);
+		
+		if(d.time() === null) {
+			console.log("sending null from natural");
+			res.send(null);
+		} else {
+			console.log("sending from natural");
+			console.log({unix : d.getTime()/1000, natural:d.toDateString()});
+			res.json({unix : d.getTime()/1000, natural:d.toDateString()});
+		}
+	} else {
+		var d = new Date(ms*1000);
+		var ret = { unix : ms, "natural" : d.toDateString() };
+		console.log("Sending from unix");
+		console.log(typeof ms);
+		console.log(ms);
+		res.send(ret);
+	}
+});
+
+port = process.env.PORT || 4040;
+
+app.listen(port, function() {
+	console.log("Ready to rock on port: "+port);
 });
